74f2481d469ca0615e5edf01639cc4bdd3624b0c
sergey-crusher <sergey_crusher@gmail.com>
2020-10-13 22:46:22 +1100

    First upload

---
+++ 1. StartWork/1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите номер задания введя цифру от 1 до 3 и нажмите ввод");
+                String valInput = Console.ReadLine();
+                Repository repository = null;
+
+                int people = 0;
+
+                switch (valInput)
+                {
+                    case "1":
+                        {
+                            people = 20;
+                            // Создание массива сотрудников
+                            repository = new Repository(people);
+                            // Печать в консоль всех сотрудников
+                            repository.Print("База данных до преобразования");
+                            // Увольнение всех Агат
+                            repository.DeleteWorkerByName("Агата");
+                            // Печать в консоль сотрудников, которые не попали под увольнение
+                            repository.Print("База данных после первого преобразования");
+                            break;
+                        }
+                    case "2":
+                        {
+                            // Создание массива сотрудников
+                            people = 20;
+                            // Более наглядно
+                            //people = 120;
+                            repository = new Repository(people);
+                            // Печать в консоль всех сотрудников
+                            repository.Print("База данных до преобразования");
+                            //Удалять пока больше 30 сотрудников
+                            while (repository.Workers.Count > 30) 
+                            {
+                                repository.DeleteWorkerByName(repository.Workers[0].FirstName);
+                            }
+                            // Печать в консоль сотрудников, которые не попали под увольнение
+                            repository.Print("База данных после первого преобразования");
+                            break;
+                        }
+                    case "3":
+                        {
+                            people = 50;
+                            // Создание массива сотрудников
+                            repository = new Repository(people);
+                            // Печать в консоль всех сотрудников
+                            repository.Print("База данных до преобразования");
+                            // Увольнение работников чья зарплата превышает 30000руб
+                            repository.DeleteWorkerBySalary(30000);
+                            // Печать в консоль сотрудников, которые не попали под увольнение
+                            repository.Print("База данных после первого преобразования");
+                            break;
+                        }
+                }
+                Console.ReadKey();
+            }
+
+            #region Домашнее задание
+
+            // Уровень сложности: просто
+            // Задание 1. Переделать программу так, чтобы до первой волны увольнени в отделе было не более 20 сотрудников
+
+            // Уровень сложности: средняя сложность
+            // * Задание 2. Создать отдел из 40 сотрудников и реализовать несколько увольнений, по результатам
+            //              которых в отделе болжно остаться не более 30 работников
+
+            // Уровень сложности: сложно
+            // ** Задание 3. Создать отдел из 50 сотрудников и реализовать увольнение работников
+            //               чья зарплата превышает 30000руб
+
+
+
+            #endregion
+
+        }
+    }
+}
---
+++ 2. LineFormat/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        /// <summary>
+        /// Класс студента
+        /// </summary>
+        class Person
+        {
+            public string   name;                                                           //имя
+            public int      age;                                                            //возраст
+            public int      height;                                                         //рост
+            public Dictionary<string, int> study = new Dictionary<string, int>();           //предметы
+
+            /// <summary>
+            /// Функция среднее арифметическое
+            /// </summary>
+            /// <returns>Среднее арифметическое</returns>
+            public double GetAverage ()
+            {
+                double avr = 0;
+                foreach (var x in study)
+                {
+                    avr += x.Value;
+                }
+                return avr/study.Count;
+            }
+
+            /// <summary>
+            /// Обычный вывод
+            /// </summary>
+            public void GetInfo1()
+            {
+                String str = "Имя: " + name + " Возраст: " + age + " Рост " + height + " Средний бал " + GetAverage().ToString("f2");
+                int centerX = (Console.WindowWidth / 2) - (str.Length / 2);
+                int centerY = (Console.WindowHeight / 2) - 1;
+                Console.SetCursorPosition(centerX, centerY);
+                Console.WriteLine(str);
+            }
+
+            /// <summary>
+            /// Форматированный вывод
+            /// </summary>
+            public void GetInfo2()
+            {
+                Console.WriteLine("Имя: {0} Возраст: {1} Рост {2} Средний бал {3:f2}",name,age,height,GetAverage());
+            }
+
+            /// <summary>
+            /// Вывод с интерполяцией строк
+            /// </summary>
+            public void GetInfo3()
+            {
+                Console.WriteLine($"Имя: {name} Возраст: {age} Рост {height} Средний бал {GetAverage():f2}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Person student = new Person();
+            student.name = "Сергей";
+            student.age = 26;
+            student.height = 178;
+            student.study.Add("История", 84);
+            student.study.Add("Математика", 100);
+            student.study.Add("Русский", 91);
+            student.GetInfo3();
+            student.GetInfo2();
+            student.GetInfo1();
+        }
+    }
+    //Условия задания
+    #region
+    // Заказчик просит написать программу «Записная книжка». Оплата фиксированная - $ 120.
+
+    // В данной программе, должна быть возможность изменения значений нескольких переменных для того,
+    // чтобы персонифецировать вывод данных, под конкретного пользователя.
+
+    // Для этого нужно: 
+    // 1. Создать несколько переменных разных типов, в которых могут храниться данные
+    //    - имя;
+    //    - возраст;
+    //    - рост;
+    //    - баллы по трем предметам: история, математика, русский язык;
+
+    // 2. Реализовать в системе автоматический подсчёт среднего балла по трем предметам, 
+    //    указанным в пункте 1.
+
+    // 3. Реализовать возможность печатки информации на консоли при помощи 
+    //    - обычного вывода;
+    //    - форматированного вывода;
+    //    - использования интерполяции строк;
+
+    // 4. Весь код должен быть откомментирован с использованием обычных и хml-комментариев
+
+    // **
+    // 5. В качестве бонусной части, за дополнительную оплату $50, заказчик просит реализовать 
+    //    возможность вывода данных в центре консоли.
+    #endregion
+}
