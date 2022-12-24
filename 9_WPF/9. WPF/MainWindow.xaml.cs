573af508c6c03d5ae845aeef5551de3c074885b5
Sergey <50127575+sergey-crusher@users.noreply.github.com>
2022-12-24 21:08:42 +1100

    9

---
+++ 9_WPF/9. WPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _9._WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert2List_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> items = textBox1.Text.Replace("  ", " ").Split(" ").ToList();
+            listBoxResult.ItemsSource = items;
+        }
+
+        private void buttonFlipPhrase_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> items = textBox2.Text.Replace("  ", " ").Split(" ").ToList().Reverse<string>().ToList();
+            labelResult.Content = items.Aggregate((i, j) => i + " " + j).ToString();
+        }
+    }
+}
--- Telegram.Bot.Examples-legacy-events/Telegram.Bot.Examples-legacy-events/Telegram.Bot.Examples.Echo/Configuration.cs
+++
@@ -1,7 +0,0 @@
-namespace Telegram.Bot.Examples.Echo
-{
-    public static class Configuration
-    {
-        public readonly static string BotToken = "1886749617:AAG2ZXvSJYYiwCiXqKRKrSFGYjl6MEUOUmA";
-    }
-}
